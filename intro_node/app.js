83ee54caead29cf994508f3d3797887b6bfc9e0c
yricarte <ryanel@indracompany.com>
2024-09-23 16:16:50 -0300

    clase 10 - contenido estático

--- intro_node/app.js
+++ intro_node/app.js
@@ -1,12 +1,26 @@
 const express = require('express');
 const app = express();
+const hbs = require('hbs')
+const path = require('path')
 const routes = require('./routes/routes');
 const port = 3000;
 
 //Middleware para servir contenido estático
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, "./public"))); 
 
+// Middleware para procesar formularios
+app.use(express.urlencoded ({extended: true}))
+
+//Archivo de rutas
 app.use('/', routes)
+
+//Configurar el motor de plantilla
+app.set('view engine', 'hbs');
+app.set("views", path.join(__dirname, "views")); 
+hbs.registerPartials(path.join(__dirname, "views", "partials")); 
+
+
+
 app.listen(port, () => {
   console.log(`Servidor escuchando en http://localhost:${port}`);
 });
--- intro_node/routes/routes.js
+++ intro_node/routes/routes.js
@@ -3,16 +3,30 @@ const path = require('path');
 const router = express.Router(); //Crear instancia router para definir las rutas
 
 //path-join ---> para construir rutas 
-router.get('/', (req, res) =>{
-  res.sendFile(path.join(__dirname + '/public/index.html'))
-})
+router.get('/', (req, res) => {
+  const usuarios = [
+    {nombre: 'Cosme', edad: 45, email: 'cosme@gmail.com'},
+    {nombre: 'Fulanito', edad: 35, email: 'fulanito@gmail.com'},
+    {nombre: 'Paquito', edad: 65, email: 'paquito@gmail.com'}
+  ]
+  res.render('index', {usuarios}); 
+});
 
-router.get('/contacto', (req, res) =>{
-  res.sendFile(path.join(__dirname + '/../public/contacto.html'))
+router.get('/formulario', (req, res) =>{
+  res.render('formulario');
 })
 
 router.get('/sobre-nosotros', (req, res) =>{
   res.sendFile(path.join(__dirname + '/../public/sobre-nosotros.html'))
+})
+
+router.get('/usuario', (req, res) =>{
+  const usuario = {
+    nombre: 'Pepito',
+    edad: 25,
+    email: 'cosme@gmail.com'
+  }
+  res.render('usuario', ({usuario}));
 })
 
 
